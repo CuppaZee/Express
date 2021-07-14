39082eade160b2533695dfcdd0fff6433bdfb1cf
Sam Hindess <mail@sohcah.dev>
2021-07-14 22:05:36 +0100

    Experimental News Widget

---
+++ android/app/src/main/java/app/cuppazee/express/NewsWidget.kt
@@ -0,0 +1,47 @@
+package app.cuppazee.express
+
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetManager.*
+import android.appwidget.AppWidgetProvider
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import android.widget.RemoteViews
+import androidx.work.*
+import java.time.Duration
+import java.util.concurrent.TimeUnit
+
+
+/**
+ * Implementation of App Widget functionality.
+ */
+class NewsWidget : AppWidgetProvider() {
+    private var lastUpdated: Long = 0
+    override fun onReceive(context: Context?, intent: Intent?) {
+        super.onReceive(context, intent)
+        if(lastUpdated < System.currentTimeMillis() - 10000) {
+            lastUpdated = System.currentTimeMillis()
+            if(intent?.action.equals(ACTION_APPWIDGET_UPDATE) || intent?.action.equals(ACTION_APPWIDGET_BIND) || intent?.action.equals(ACTION_APPWIDGET_OPTIONS_CHANGED) || intent?.action.equals(ACTION_APPWIDGET_CONFIGURE) || intent?.action.equals(ACTION_APPWIDGET_ENABLED))
+            {
+                val appWidgetManager = getInstance(context)
+                val thisAppWidgetComponentName = ComponentName(context!!.packageName, javaClass.name)
+                val appWidgetIds = appWidgetManager.getAppWidgetIds(thisAppWidgetComponentName)
+
+
+                for (appWidgetId in appWidgetIds) {
+                    val uploadWorkRequest: WorkRequest =
+                            OneTimeWorkRequestBuilder<NewsWidgetWorker>()
+                                    .setInputData(workDataOf(
+                                            "APP_WIDGET_ID" to appWidgetId
+                                    ))
+                                    .build()
+                    WorkManager
+                            .getInstance(context)
+                            .enqueue(uploadWorkRequest)
+                    Log.i("CZNewsWidget", "Queueing Update for ID: $appWidgetId")
+                }
+            }
+        }
+    }
+}
---
+++ android/app/src/main/java/app/cuppazee/express/NewsWidgetData.kt
@@ -0,0 +1,93 @@
+package app.cuppazee.express
+
+import android.app.PendingIntent
+import android.appwidget.AppWidgetManager
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.util.Log
+import android.view.View
+import android.widget.RemoteViews
+import androidx.concurrent.futures.CallbackToFutureAdapter.getFuture
+import androidx.work.ListenableWorker
+import androidx.work.WorkerParameters
+import com.android.volley.Request
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley.newRequestQueue
+import com.google.common.util.concurrent.ListenableFuture
+import com.squareup.picasso.Picasso
+import org.json.JSONArray
+import org.json.JSONException
+import org.json.JSONObject
+
+
+class NewsWidgetWorker(appContext: Context, workerParams: WorkerParameters):
+        ListenableWorker(appContext, workerParams) {
+
+    override fun startWork(): ListenableFuture<Result> {
+        return getFuture {
+            // Do the work here--in this case, upload the images.
+            val appWidgetId = inputData.getInt("APP_WIDGET_ID", -1)
+            val appWidgetManager = AppWidgetManager.getInstance(applicationContext)
+
+            if (appWidgetId < 0) {
+                it.set(Result.failure())
+            } else {
+                val queue = newRequestQueue(applicationContext)
+
+                val url = "https://server.cuppazee.app/widget/news?timestamp=" + System.currentTimeMillis()
+
+                val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, url, null,
+                        { response ->
+                            Log.i("CZNewsWidget", response.toString())
+                            val views = RemoteViews(applicationContext.packageName, R.layout.news_widget)
+
+                            try {
+                                val data: JSONArray = response.getJSONArray("data")
+                                val news_a: JSONObject = data.getJSONObject(0)
+                                val news_b: JSONObject = data.getJSONObject(1)
+
+                                // Entry A
+                                views.setViewVisibility(R.id.news_widget_image_a, View.VISIBLE)
+                                Picasso.get().load(news_a.getString("image_url")).into(views, R.id.news_widget_image_a, intArrayOf(appWidgetId))
+                                views.setTextViewText(R.id.news_widget_text_a, news_a.getString("title"))
+                                // OnClick
+                                val intent_a = Intent(Intent.ACTION_VIEW, Uri.parse(news_a.getString("blog_url")))
+                                val pendingIntent_a = PendingIntent.getActivity(applicationContext, 0, intent_a, 0)
+                                views.setOnClickPendingIntent(R.id.news_widget_a, pendingIntent_a)
+
+                                // Entry B
+                                views.setViewVisibility(R.id.news_widget_image_b, View.VISIBLE)
+                                Picasso.get().load(news_b.getString("image_url")).into(views, R.id.news_widget_image_b, intArrayOf(appWidgetId))
+                                views.setTextViewText(R.id.news_widget_text_b, news_b.getString("title"))
+                                // OnClick
+                                val intent_b = Intent(Intent.ACTION_VIEW, Uri.parse(news_b.getString("blog_url")))
+                                val pendingIntent_b = PendingIntent.getActivity(applicationContext, 0, intent_b, 0)
+                                views.setOnClickPendingIntent(R.id.news_widget_b, pendingIntent_b)
+                                appWidgetManager.updateAppWidget(appWidgetId, views)
+                                it.set(Result.success())
+                            } catch (error: JSONException) {
+                                views.setViewVisibility(R.id.news_widget_image_a, View.INVISIBLE)
+                                views.setViewVisibility(R.id.news_widget_image_b, View.INVISIBLE)
+                                views.setTextViewText(R.id.news_widget_text_a, "JSON Error")
+                                views.setTextViewText(R.id.news_widget_text_b, "Please report this to CuppaZee: $error")
+                                appWidgetManager.updateAppWidget(appWidgetId, views)
+                                it.set(Result.failure())
+                            }
+                        },
+                        { error ->
+                            val views = RemoteViews(applicationContext.packageName, R.layout.news_widget)
+                            views.setViewVisibility(R.id.news_widget_image_a, View.INVISIBLE)
+                            views.setViewVisibility(R.id.news_widget_image_b, View.INVISIBLE)
+                            views.setTextViewText(R.id.news_widget_text_a, "JSON Error")
+                            views.setTextViewText(R.id.news_widget_text_b, "Please report this to CuppaZee: $error")
+                            appWidgetManager.updateAppWidget(appWidgetId, views)
+                            it.set(Result.failure())
+                        }
+                )
+
+                queue.add(jsonObjectRequest)
+            }
+        }
+    }
+}
